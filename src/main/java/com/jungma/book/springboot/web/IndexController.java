905fba56cb9e05349113b402e2c02a16e83f505f
Jungma <mbjun3630@naver.com>
2023-04-30 14:26:10 +0900

    feat: annotation resolver

--- src/main/java/com/jungma/book/springboot/web/IndexController.java
+++ src/main/java/com/jungma/book/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.jungma.book.springboot.web;
 
+import com.jungma.book.springboot.config.auth.LoginUser;
 import com.jungma.book.springboot.config.auth.dto.SessionUser;
 import com.jungma.book.springboot.service.post.PostService;
 import com.jungma.book.springboot.web.dto.PostResponse;
@@ -18,11 +19,8 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) { // Model: 서버 템플릿 엔진에서 사용할 수 있는 객체를 저장할 수 있음
+    public String index(@LoginUser SessionUser user, Model model) { // Model: 서버 템플릿 엔진에서 사용할 수 있는 객체를 저장할 수 있음
         model.addAttribute("posts", postService.findAllDesc());
-
-        // CustomOAuth2UserService 에서 로그인 성공 시 세션에 SessionUser 를 저장하도록 구성
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
 
         // 세션에 저장된 값이 있을 때만 model 에 userName 으로 등록
         if (user != null) {
